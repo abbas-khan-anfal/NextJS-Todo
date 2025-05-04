e9967e1054eb10049e401007a9d013918f2cc3fd
Abbas Khan <ak9950025@gmail.com>
2025-05-04 18:30:55 +0500

    corrected minor errors

--- app/hooks/useFunctional.js
+++ app/hooks/useFunctional.js
@@ -1,30 +1,35 @@
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// you can use hook like below, but here it doesn't need in my app, so that's why i just show that how we can use hook.
 
-export const navToggler = () => {
-    console.log("Hello world, hook called!");
-    return;
-}
 
-export const fetchApiData = () => {
+
+// 
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// export const useNavToggler = () => {
+//     console.log("Hello world, hook called!");
+//     return;
+// }
+
+// export const useFetchApiData = () => {
     
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            try
-            {
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-                setData(response.data || []);
-            }
-            catch(error)
-            {
-                console.log(`Error fetching data : ${error.message}`);
-            }
-        }
-        fetchData();
-    },[]);
+//     const [data, setData] = useState([]);
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             try
+//             {
+//                 const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+//                 setData(response.data || []);
+//             }
+//             catch(error)
+//             {
+//                 console.log(`Error fetching data : ${error.message}`);
+//             }
+//         }
+//         fetchData();
+//     },[]);
 
-    return data;
+//     return data;
 
-}
+// }
--- app/layout.js
+++ app/layout.js
@@ -14,13 +14,13 @@ export default function RootLayout({ children }) {
 
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
+      {/* fontawesome cdn link */}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+
       {/* bootstrap cdn link */}
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossOrigin="anonymous" />
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossOrigin="anonymous"></script>
-
-      {/* fontawesome cdn link */}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossOrigin="anonymous"></script>  
 
       
       </head>
